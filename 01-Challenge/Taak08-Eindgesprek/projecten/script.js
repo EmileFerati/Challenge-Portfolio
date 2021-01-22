82d7436fade7e8d9747d422ac82de500802f8cd7
emile ferati <emile.ferati@student.rocva.nl>
2021-01-22 13:02:01 +0100

    update alles

---
+++ 01-Challenge/Taak08-Eindgesprek/projecten/script.js
@@ -0,0 +1,55 @@
+
+        prompt("wat is je voornaam?")
+        prompt("wat is je achternaam?")
+        prompt("wat is je emailadres?")
+        prompt("wat is je woonplaats?")
+
+
+
+let leeftijd = prompt ("wat is je leeftijd");
+let stemleeftijd = 17;
+
+  // TRUE or FALSE
+if(leeftijd >= stemleeftijd){
+ //Als de gebruiker (bezoeker) 16 jaar dan wordt de achtergrond kleur van index.html een oranje-achtige kleur
+
+}
+document.querySelector("body").style.backgroundColor = 'orange';
+
+//TODO: input van gebruiker in obejct persoon opslaan, alle 5 eigenschappen erbij.
+let persoon = {
+    voornaam : prompt("Wat is je naam?"),
+    achternaam : prompt("Wat is je achternaam?"),
+    leeftijd : prompt("Wat is je leeftijd?"),
+
+   
+};
+console.log(persoon);
+console.log(persoon.achternaam);
+
+
+    
+
+// let persoonelement = document.querySelector(".container")
+// persoonelement.innerHTML = "Emile Ferati"
+
+let mijnLeeftijd= prompt("wat is je leeftijd?");
+let stemGerechtigheid= 18
+let oranjekleur= 16
+let blauwkleur= 17
+let groenkleur= 19
+if (persoon.leeftijd==stemGerechtigheid){
+   // Als de gebruiker (bezoeker) 16 jaar dan wordt de achtergrond kleur van index.html een oranje-achtige kleur
+   document.querySelector("body").style.backgroundColor="gold"
+}
+
+if (mijnLeeftijd==oranjekleur){
+   document.querySelector("body").style.backgroundColor="orange"
+}
+if (mijnLeeftijd==blauwkleur){
+   document.querySelector("body").style.backgroundColor="blue"
+}
+if (mijnLeeftijd==groenkleur){
+   document.querySelector("body").style.backgroundColor="green"
+}
+
